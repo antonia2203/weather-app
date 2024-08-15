41fb94a60c205e42d14284d6b687ed5ec61f4eee
MihaiAntoniaMaria <mihai_antoniamaria@yahoo.com>
2024-08-15 18:22:04 +0300

    trying to make the app

--- src/main/java/com/weatherapp/base/model/WeatherDTO.java
+++ src/main/java/com/weatherapp/base/model/WeatherDTO.java
@@ -1,38 +1,28 @@
 package com.weatherapp.base.model;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 
 @Entity
 @Table(name = "weather")
-public class WeatherDTO {
+public class WeatherDTO extends BasicWeatherDTO {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotNull
-    @Size(min = 1, max = 100)
-    @Column(name = "city", nullable = false)
-    private String city;
-
     @Enumerated(EnumType.STRING)
     @Column(name = "weather_condition", nullable = false)
     private WeatherCondition weatherCondition;
 
-    @Column(name = "temperature", nullable = false)
-    private Double temperature;
-
     // Default constructor for JPA
     public WeatherDTO() {
+        super();
     }
 
     public WeatherDTO(Long id, String city, WeatherCondition weatherCondition, Double temperature) {
+        super(city, temperature);
         this.id = id;
-        this.city = city;
         this.weatherCondition = weatherCondition;
-        this.temperature = temperature;
     }
 
     public Long getId() {
@@ -43,14 +33,6 @@ public class WeatherDTO {
         this.id = id;
     }
 
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
     public WeatherCondition getWeatherCondition() {
         return weatherCondition;
     }
@@ -59,21 +41,13 @@ public class WeatherDTO {
         this.weatherCondition = weatherCondition;
     }
 
-    public Double getTemperature() {
-        return temperature;
-    }
-
-    public void setTemperature(Double temperature) {
-        this.temperature = temperature;
-    }
-
     @Override
     public String toString() {
         return "WeatherDTO{" +
                 "id=" + id +
-                ", city='" + city + '\'' +
+                ", city='" + getCity() + '\'' +
                 ", weatherCondition=" + weatherCondition +
-                ", temperature=" + temperature +
+                ", temperature=" + getTemperature() +
                 '}';
     }
 }
